0a4277de528501817d59f4c24334af1b048997a7
kenfj <44661517+kenfj@users.noreply.github.com>
2025-01-12 17:48:53 +0900

    Add comment official doc url of the prisma client code

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,3 +6,6 @@ export const prisma =
   globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// This code is from the official documentation
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
