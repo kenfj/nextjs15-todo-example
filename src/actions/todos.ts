c197e02026b016e83549b22f1d112dfad1f6b1c7
kenfj <44661517+kenfj@users.noreply.github.com>
2025-01-22 23:57:47 +0900

    fix lint error

--- src/actions/todos.ts
+++ src/actions/todos.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-import { TodoFormState, DeleteTodoState } from '@/models/todo';
+import { DeleteTodoState, TodoFormState } from '@/models/todo';
 import { deleteTodo, saveTodo } from '@/services/todoService';
 import { getCookie } from '@/utils/cookieUtils';
 
