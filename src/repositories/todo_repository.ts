a7db8fd72be32aa0d4563d0165e46a568baded3b
kenfj <44661517+kenfj@users.noreply.github.com>
2025-02-05 00:45:27 +0900

    Update `findAllByUserId` and `deleteTodoById` to handle `userId` as `string | undefined`
    * **findAllByUserId**
      - Change argument `userId` to `string | undefined`
      - Add check for `userId` and throw error if `userId` is `undefined`
    
    * **deleteTodoById**
      - Change argument `userId` to `string | undefined`
      - Add check for `userId` and throw error if `userId` is `undefined`

--- src/repositories/todo_repository.ts
+++ src/repositories/todo_repository.ts
@@ -6,10 +6,16 @@ export async function createTodo(input: Prisma.TodoCreateInput): Promise<Todo> {
   return await prisma.todo.create({ data: { ...input } });
 }
 
-export async function findAllByUserId(userId: number, orderBy: keyof Todo = 'createdAt'): Promise<Todo[]> {
+export async function findAllByUserId(userId: string | undefined, orderBy: keyof Todo = 'createdAt'): Promise<Todo[]> {
+  if (userId === undefined) {
+    throw new Error("User ID is undefined");
+  }
   return await prisma.todo.findMany({ where: { userId }, orderBy: { [orderBy]: 'asc' } });
 }
 
-export async function deleteTodoById(todoId: number, userId: number): Promise<Todo> {
+export async function deleteTodoById(todoId: number, userId: string | undefined): Promise<Todo> {
+  if (userId === undefined) {
+    throw new Error("User ID is undefined");
+  }
   return await prisma.todo.delete({ where: { id: todoId, userId } });
 }
