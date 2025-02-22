148810113f3869ed52008e14b509b6b185ca26f2
kenfj <44661517+kenfj@users.noreply.github.com>
2025-02-23 00:04:17 +0900

    Rename and update import paths for `auth-button.tsx`
    * **Rename and move file**
      - Rename `src/components/auth-button.tsx` to `src/components/auth/auth-button.tsx`
    * **Update import paths**
      - Update import paths for `SignIn` and `SignOut` components in `auth-button.tsx`
      - Update import path for `auth` in `auth-button.tsx`
      - Update import path for `AuthButton` in `navbar.tsx`
      - Update import path for `authConfig` in `middleware.ts`

--- src/components/auth/auth-button.tsx
+++ src/components/auth/auth-button.tsx
@@ -1,6 +1,6 @@
-import { auth } from "@/lib/auth/auth";
 import SignIn from "@/components/auth/sign-in";
 import SignOut from "@/components/auth/sign-out";
+import { auth } from "@/lib/auth/auth";
 
 const AuthButton = async () => {
   const session = await auth();
